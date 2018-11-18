f502baab103b90f30acfd6396c0fda5f7e98ed9d
kazu-taka <k.takahashi@nowall.co.jp>
2018-11-18 09:21:19 +0900

    関数定義を学んだ

---
+++ app.py
@@ -0,0 +1,27 @@
+# def hello():
+#     print("Hello World")
+#
+#
+# hello()
+#
+#
+# def say_hello(name):
+#     print(f"Hi {name}")
+#
+#
+# say_hello("Bob")
+#
+#
+# def double(number):
+#     return 2 * number
+#
+#
+# result = double(3)
+# print(result)
+
+
+def str_combine(name1, name2):
+    return f"{name1} {name2}"
+
+
+print(str_combine("Kazuma", "Takahashi"))
